32154d604a798f467484231041f674442e33d50b
Aditya <abhat@wpi.edu>
2015-11-04 15:30:29 -0500

    Added method for stitching matrix together

--- src/Robot.py
+++ src/Robot.py
@@ -1,12 +1,20 @@
 import numpy as np
 
 class Robot:
-    perceptsize=3
+    perceptradius=3
     #TODO: Change this based on the size of the world
     perceptmap = np.zeros(shape=(1000, 1000), dtype=int)
     #This position has to be updated on every move
-    xmapposition=250
-    ymapposition=250
+    xmapposition=500
+    ymapposition=500
+
+    #This value has to be updated with the minimum value of x and y reached by the robot
+    minxposition=499
+    minyposition=499
+
+    #This value has to be updated with the minimum value of x and y reached by the robot
+    maxxposition=501
+    maxyposition=501
 
     def expandperceptmap(self, perceptMatrix):
         """Given the percept matrix, the robot adds the percept to the map of the robot"""
@@ -16,3 +24,27 @@ class Robot:
         for i in range(self.perceptsize):
             for j in range(self.perceptsize):
                 self.perceptmap[mapstartX+i][mapstartY+j]=perceptMatrix[i][j]
+
+    #Given 2 robots in proximity, the map of the other robot is taken and stitched to the current map to make a larger map of the environment
+    def stitchmaps(self, relativePositionOfOtherRobot, otherRobot):
+        """Given 2 robots in proximity, the map of the other robot is taken and stitched to the current map to make a larger map of the environment"""
+        rpositionOfOtherRobotX = otherRobot.xmapposition-otherRobot.minxposition
+        rpositionOfOtherRobotY = otherRobot.ymapposition-otherRobot.minyposition
+        robotmap=otherRobot.perceptmap[otherRobot.minxposition:otherRobot.maxxposition, otherRobot.minyposition:otherRobot.maxyposition]
+        shapeofworld=np.shape(robotmap)
+        positionxofotherrobot=self.xmapposition-self.perceptradius+relativePositionOfOtherRobot[0]
+        positionyofotherrobot=self.ymapposition-self.perceptradius+relativePositionOfOtherRobot[1]
+        startmapx=positionxofotherrobot-rpositionOfOtherRobotX
+        startmapy=positionyofotherrobot-rpositionOfOtherRobotY
+        self.perceptmap[startmapx:startmapx+shapeofworld[0], startmapy:startmapy+shapeofworld[1]]=robotmap
+
+        #Update the min and max positions covered by the map
+        if self.minxposition>startmapx:
+            self.minxposition=startmapx
+        if self.minyposition>startmapy:
+            self.minyposition=startmapy
+
+        if self.maxxposition<startmapy+shapeofworld[0]:
+            self.maxxposition=startmapy+shapeofworld[0]
+        if self.maxyposition>startmapy+shapeofworld[1]:
+            self.maxyposition=startmapy+shapeofworld[1]
