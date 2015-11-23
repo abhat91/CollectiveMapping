Added check for correct movement for gradient add

src/main.py
@@ -25,7 +25,7 @@ def readmap():
 selectedrobot = 0
 previousRobot = 0
 worldmap=readmap()
-world=world.World(np.array(worldmap), [(5,3),(5,4),(15,15),(15,1),(1,15),(1,16),(1,17),(2,15),(2,15),(0,15),(0,5),(11,15),(12,15),(12,13),(1,15),(14,1),(17,1),(15,15),(1,1),(5,3),(5,4),(15,15),(15,1),(1,15),(1,16),(1,17),(2,15),(2,15),(0,15),(0,5),(11,15),(12,15),(12,13),(1,15),(14,1),(17,1),(15,15),(1,1)])
+world=world.World(np.array(worldmap), [(5,3),(5,4),(15,15),(15,1),(1,15),(1,16),(1,17),(2,15),(2,15),(0,15),(0,5)])
 showmap=graphics.Graphics(len(worldmap))
 for i in range(0,len(world.robotsbypos)):
     showmap.listbox.insert(tk.END,str(i))
@@ -41,11 +41,11 @@ def run(t):
         t = t + 1
         for i in range(len(world.posbyrobots.keys())):
             robot = world.posbyrobots.keys()[i]
-            if robot.bayesMove()=='Explored':
+            if robot.gradientmove()=='Explored':
                 flag=True
                 selectedrobot = i
                 print "explored"
-            #robot.gradientmove()
+            #robot.bayesMove()
     if not flag and len(showmap.listbox.curselection()) > 0:
         selectedrobot = int(showmap.listbox.curselection()[0])
     if previousRobot != selectedrobot: